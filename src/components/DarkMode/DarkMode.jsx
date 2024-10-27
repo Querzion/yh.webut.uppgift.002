357b81402d3ac1de8450a21ad3f62023c3ca6c0a
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-10-27 02:03:23 +0200

    dark-mode up and running.

---
+++ src/components/DarkMode/DarkMode.jsx
@@ -0,0 +1,56 @@
+// src/components/DarkMode/DarkMode.jsx
+import React, { useEffect, useState } from 'react';
+
+import SiliconLogoTextDark from "/src/assets/images/mainpage-dark/navbar/silicon-text-dark.svg";
+import SiliconLogoTextLight from "/src/assets/images/mainpage-light/navbar/silicon-text-light.svg";
+import appleAppStoreDark from "/src/assets/images/mainpage-dark/showcase/appstore-dark.svg";
+import appleAppStoreLight from "/src/assets/images/mainpage-light/showcase/appstore-light.svg";
+import googlePlayStoreDark from "/src/assets/images/mainpage-dark/showcase/googleplay-dark.svg";
+import googlePlayStoreLight from "/src/assets/images/mainpage-light/showcase/googleplay-light.svg";
+
+const DarkMode = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    // Effect to update body class and logo images based on isDarkMode
+    useEffect(() => {
+        const element = document.body;
+        if (isDarkMode) {
+            element.classList.add("dark-mode");
+        } else {
+            element.classList.remove("dark-mode");
+        }
+
+        // Update logo images
+        const logo = document.getElementById('logo-text');
+        logo.src = isDarkMode ? SiliconLogoTextDark : SiliconLogoTextLight;
+
+        const appstore_logotype = document.getElementById('appstore');
+        appstore_logotype.src = isDarkMode ? appleAppStoreDark : appleAppStoreLight;
+
+        const playstore_logotype = document.getElementById('playstore');
+        playstore_logotype.src = isDarkMode ? googlePlayStoreDark : googlePlayStoreLight;
+    }, [isDarkMode]);
+
+    // Function to toggle dark mode
+    const toggleDarkMode = () => {
+        setIsDarkMode(prevMode => !prevMode);
+    };
+
+    return (
+        <div id="darkmode-toggle-switch" className="btn-toggle-switch">
+            <span className="label">Dark mode</span>
+            <label htmlFor="darkmode-switch" className="toggle-switch">
+                <input 
+                    id="darkmode-switch" 
+                    type="checkbox" 
+                    checked={isDarkMode} 
+                    onChange={toggleDarkMode} 
+                    aria-label="dark-mode input toggle" 
+                />
+                <span className="slider round"></span>
+            </label>
+        </div>
+    );
+};
+
+export default DarkMode;
--- src/sections/Header/Navbar/Navbar.jsx
+++ src/sections/Header/Navbar/Navbar.jsx
@@ -1,39 +1,41 @@
 import React from 'react'
 
 import SiliconLogo from "../../../assets/images/mainpage-light/navbar/silicon-logo.svg"
-import SiliconLogoText from "../../../assets/images/mainpage-light/navbar/silicon-text-light.svg"
+import DarkMode from '../../../components/DarkMode/DarkMode'
 
 const Navbar = () => {
 
     return (
         <section id="navbar" className="sticky-header">
 
-        <a id="logo" href="/index.html">
-            <img id="logo-img" src={SiliconLogo} alt="Silicon Logotype" />
-            <img id="logo-text" src={SiliconLogoText} alt="Text Silicon" />
-        </a>
-        
-        <nav id="main-menu" className="navbar">
-            <a className="nav-link" href="#brands">Features</a>
-            <a className="nav-link" href="./contact.html">Contact</a>
-        </nav>
-        
-        <div id="darkmode-toggle-switch" className="btn-toggle-switch">
-            <span className="label">Dark mode</span>
-            <label htmlFor="darkmode-switch" className="toggle-switch">
-                <input id="darkmode-switch" type="checkbox" aria-label="dark-mode input toggle" />
-                <span className="slider round"></span>
-            </label>
-        </div>
-        
-        <a id="auth-signin-create" href="#" className="btn-primary">
-            <i className="fa-regular fa-user" aria-hidden="true"></i>
-            <span>Sign in / up</span>
-        </a>
-        
-        <button className="btn-mobile" title="Menu borked"  aria-label="menu button">
-            <i className="fa-solid fa-bars"></i>
-        </button>
+            <a id="logo" href="/index.html">
+                <img 
+                    id="logo-img" 
+                    src={SiliconLogo} 
+                    alt="Silicon Logotype" 
+                />
+                <img 
+                    id="logo-text" 
+                    // src={SiliconLogoTextLight} 
+                    alt="Text Silicon" 
+                />
+            </a>
+            
+            <nav id="main-menu" className="navbar">
+                <a className="nav-link" href="#brands">Features</a>
+                <a className="nav-link" href="./contact.html">Contact</a>
+            </nav>
+            
+            <DarkMode />
+            
+            <a id="auth-signin-create" href="#" className="btn-primary">
+                <i className="fa-regular fa-user" aria-hidden="true"></i>
+                <span>Sign in / up</span>
+            </a>
+            
+            <button className="btn-mobile" title="Menu borked"  aria-label="menu button">
+                <i className="fa-solid fa-bars"></i>
+            </button>
         
         </section>
     )
